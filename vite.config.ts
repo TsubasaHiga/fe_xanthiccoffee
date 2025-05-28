8efc8d1236d8167cd2603abe4ffa85fb083cb38e
TsubasaHiga <info@cofus.work>
2025-05-29 04:27:53 +0900

    feat: add analyze mode support with rollup-plugin-visualizer in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,32 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { type UserConfig, defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    host: true,
-    port: 5173
-  },
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+const config = (mode: string): UserConfig => {
+  return {
+    server: {
+      host: true,
+      port: 5173
+    },
+    plugins: [
+      react(),
+      tailwindcss(),
+      mode === 'analyze' &&
+        visualizer({
+          open: true,
+          filename: './analyze/stats.html',
+          gzipSize: true
+        })
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     }
   }
-})
+}
+
+export default defineConfig(({ mode }) => config(mode))
